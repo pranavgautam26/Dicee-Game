3043a3bc5d86c49fa8a30358cb9f6d595509e792
pranav gautam <63493042+pranavgautam26@users.noreply.github.com>
2020-09-12 15:15:23 +0530

    added simple javascript game files

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+// Generating a random number for player1
+
+  var randomNumber1 = Math.random();
+  randomNumber1 = randomNumber1 * 6;
+  randomNumber1 = Math.floor(randomNumber1) + 1;
+
+
+
+
+  // Generating a random number for player 2
+
+
+  var randomNumber2 = Math.random();
+  randomNumber2 = randomNumber2 * 6;
+  randomNumber2 = Math.floor(randomNumber2) + 1;
+
+  // setting images according to the random numbers
+
+  document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+  document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+
+
+  // conditions for the selection of the winner
+
+  if(randomNumber1 > randomNumber2)
+  {
+    document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
+  }
+  else if(randomNumber1 < randomNumber2)
+  {
+    document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
+  }
+  else{
+    document.querySelector("h1").innerHTML="🚩 It's Draw! 🚩";
+  }
